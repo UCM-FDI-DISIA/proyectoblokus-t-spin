dadc330df81904a92778e8418e326a2b785421a9
luisolUCM <luisolis@ucm.es>
2021-04-18 21:52:39 +0200

    carga de imagenes en el tablero

--- Blokus/src/view/TableroPanel.java
+++ Blokus/src/view/TableroPanel.java
@@ -1,8 +1,12 @@
 package view;
 
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 import controller.Controller;
@@ -12,6 +16,11 @@ import model.Jugador;
 public class TableroPanel extends JPanel implements GameObserver{
 	private static final long serialVersionUID = 1L;
 	
+	private Image _casillaVacia;
+	private Image _casillaRoja;
+	private Image _casiilaAzul;
+	private Image _casillaVerde;
+	private Image _casillaAmarilla;
 	
 	TableroPanel(Controller ctrl) {
 		initGUI();
@@ -21,7 +30,11 @@ public class TableroPanel extends JPanel implements GameObserver{
 
 	private void initGUI() {
 		// TODO Auto-generated method stub
-		
+		_casillaVacia = loadImage("");
+		_casillaRoja = loadImage("");
+		_casiilaAzul = loadImage("");
+		_casillaVerde = loadImage("");
+		_casillaAmarilla = loadImage("");
 	}
 
 
@@ -36,6 +49,15 @@ public class TableroPanel extends JPanel implements GameObserver{
 	public void onFichaAnadida(List<Jugador> jugadores, HashMap<String, String> mapaCasillas) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	private Image loadImage(String img) {
+		Image i = null;
+		try {
+			return ImageIO.read(new File("resources/" + img));
+		} catch (IOException e) {
+		}
+		return i;
 	}
 
 }
