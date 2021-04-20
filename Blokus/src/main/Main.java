5bd35edb9a6861e114ad59df923586d462bc1c8a
Sodoka <alexandracideca@gmail.com>
2021-04-20 11:50:26 +0200

    menu

--- Blokus/src/main/Main.java
+++ Blokus/src/main/Main.java
@@ -1,117 +1,82 @@
 package main;
 import model.Ficha;
+import model.Game;
+import view.MainWindow;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.security.PrivateKey;
 import javax.swing.*;
-
-public class Main extends JFrame implements ActionListener{
-
-//	private JPanel mainPanel;
-//	private  JPanel sidePanel;
-//	private JPanel piecesPanel;
-//	private JPanel boardPanel;
-//	private JLabel label;
-//	private ImageIcon boardImage;
-//	private JButton izq;
-
-	JLabel titulo =new JLabel();
-	JButton p1=new JButton();
-	JButton p2=new JButton();
-	JButton p3=new JButton();
-	JButton p4=new JButton();
+import java.util.*;
 
 
+import controller.Controller;
+
+public class Main {
+	
+	private static String _mode="gui";
+
+
+	private static void startBatchMode() throws IOException {
+		
+   	Controller controller;
+    	Game game;
+    	Scanner scanner;
+    	int numJugadores = 2;
+    	String[] args = null;
+		//TEST
+    	if (args.length == 1) {
+       	numJugadores = Integer.parseInt(args[0]);
+    	}
+//    	//TEST
+    	scanner = new Scanner(System.in);
+    	game = new Game(numJugadores);
+    	controller = new Controller(game, scanner);
+   		controller.run(); //Ejecuta el juego
+
+   		System.out.print("Game closed.");
+//    	//TEST ZONE
+
+//    	//TEST ZONE
+	}
+	
+	private static void startGUIMode () throws IOException{
+		
+		Controller controller = null;
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {	new MainWindow(controller);	}
+			
+			
+		});
+		
+	}
+	
+	private static void start(String[] args) throws IOException{
+		
+			if(_mode=="console") {
+			startBatchMode();
+				}else {
+					startGUIMode();
+				}
+		
+	}
+	
 	public static void main(String[] args)  {
 
+		try {
+			start(args);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 
-		Main menu= new Main();
 
-//    	Controller controller;
-//    	Game game;
-//    	Scanner scanner;
-//    	int numJugadores = 2;
-//    	//TEST
-//    	if (args.length == 1) {
-//        	numJugadores = Integer.parseInt(args[0]);
-//    	}
-//    	//TEST
-//    	scanner = new Scanner(System.in);
-//    	game = new Game(numJugadores);
-//    	controller = new Controller(game, scanner);
-//    	controller.run(); //Ejecuta el juego
-//
-//    	System.out.print("Game closed.");
-//    	//TEST ZONE
-//
-//    	//TEST ZONE
     }
 
-
-
-    public Main(){
-		this.setTitle("Blokus");
-		this.setSize(800,800);
-		this.setResizable(false);
-		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.creaElementos();
-		this.setLayout(null);
-		this.setVisible(true);
-	}
 //
-	public void creaElementos(){
-
-		titulo.setBounds(310,250,2000,30);
-		titulo.setFont(new Font("comic-sans", 1, 21));
-		titulo.setText("Numero de Jugadores");
-		this.getContentPane().add(titulo);
-
-
-		p1.setBounds(350,300,120,30);
-		p1.setText("2 Jugadores");
-		this.getContentPane().add(p1);
-
-		p2.setBounds(350,350,120,30);
-		p2.setText("3 Jugadores");
-		this.getContentPane().add(p2);
-
-		p3.setBounds(350,400,120,30);
-		p3.setText("4 Jugadores");
-		this.getContentPane().add(p3);
-
-
-		p4.setBounds(350,450,120,30);
-		p4.setText("Salir");
-		this.getContentPane().add(p4);
-
-		p1.addActionListener(this);
-		p2.addActionListener(this);
-		p3.addActionListener(this);
-		p4.addActionListener(this);
-
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-
-		if(e.getSource()==p1){
-			//tablero para dos jugadores
-			//v1.setVisible(true);
-		}
-		if(e.getSource()==p2){
-			//tablero para Tres jugadores
-			//v2.setVisible(true):
-		}
-		if(e.getSource()==p3){
-			//tablero para cuatro jugadores
-			//v3.setVisible(true):
-		}
-		if(e.getSource()==p4){
-			System.exit(WIDTH);
-		}
-	}
+	
 }
 
---
+++ Blokus/src/view/MainWindow.java
@@ -0,0 +1,92 @@
+package view;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import controller.Controller;
+
+public class MainWindow extends JFrame implements ActionListener{
+	
+	JLabel titulo =new JLabel();
+	JButton p1=new JButton();
+	JButton p2=new JButton();
+	JButton p3=new JButton();
+	JButton p4=new JButton();
+
+private Controller _controller;
+
+public MainWindow(Controller controller) {
+	super("Blockus");
+	
+	_controller=controller;
+	initGUI();
+}
+
+private void initGUI(){
+	this.setTitle("Blokus");
+	this.setSize(800,800);
+	this.setResizable(false);
+	this.setLocationRelativeTo(null);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	this.creaElementos();
+	this.setLayout(null);
+	this.setVisible(true);
+}
+
+public void creaElementos(){
+
+	titulo.setBounds(310,250,2000,30);
+	titulo.setFont(new Font("comic-sans", 1, 21));
+	titulo.setText("Numero de Jugadores");
+	this.getContentPane().add(titulo);
+
+
+	p1.setBounds(350,300,120,30);
+	p1.setText("2 Jugadores");
+	this.getContentPane().add(p1);
+
+	p2.setBounds(350,350,120,30);
+	p2.setText("3 Jugadores");
+	this.getContentPane().add(p2);
+
+	p3.setBounds(350,400,120,30);
+	p3.setText("4 Jugadores");
+	this.getContentPane().add(p3);
+
+
+	p4.setBounds(350,450,120,30);
+	p4.setText("Salir");
+	this.getContentPane().add(p4);
+
+	p1.addActionListener(this);
+	p2.addActionListener(this);
+	p3.addActionListener(this);
+	p4.addActionListener(this);
+
+}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+
+	if(e.getSource()==p1){
+		//tablero para dos jugadores
+		//v1.setVisible(true);
+	}
+	if(e.getSource()==p2){
+		//tablero para Tres jugadores
+		//v2.setVisible(true):
+	}
+	if(e.getSource()==p3){
+		//tablero para cuatro jugadores
+		//v3.setVisible(true):
+	}
+	if(e.getSource()==p4){
+		System.exit(WIDTH);
+	}
+}
+}
