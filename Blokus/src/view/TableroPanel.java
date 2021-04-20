8a8760356059c3f0787ae3be19a7033caa92613e
luisolUCM <luisolis@ucm.es>
2021-04-20 22:20:34 +0200

    Boceto tablero
    Clickar en las fichas no hace nada

--- Blokus/src/view/TableroPanel.java
+++ Blokus/src/view/TableroPanel.java
@@ -7,6 +7,8 @@ import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
@@ -20,18 +22,13 @@ import controller.Controller;
 import model.GameObserver;
 import model.Jugador;
 
+
 public class TableroPanel extends JPanel implements GameObserver{
 	private static final long serialVersionUID = 1L;
 
-	private static final Color _COLOR_FONDO = Color.WHITE;
-	
 	private static final int SQUARESIZE = 40;
-	private JButton tablero[][];
-	private Image _casillaVacia;
-	private Image _casillaRoja;
-	private Image _casiilaAzul;
-	private Image _casillaVerde;
-	private Image _casillaAmarilla;
+	
+
 	private int width = 20, height = 20;
 	
 	
@@ -39,7 +36,6 @@ public class TableroPanel extends JPanel implements GameObserver{
 		super(new GridLayout(20, 20));
 		this.setPreferredSize(new Dimension(width * SQUARESIZE, height
 				* SQUARESIZE));
-		tablero = new JButton[height][width];
 		
 		initGUI();
 
@@ -48,27 +44,14 @@ public class TableroPanel extends JPanel implements GameObserver{
 
 
 	private void initGUI() {
-
-		_casillaVacia = loadImage("casilla_vacia.png");
-		_casillaRoja = loadImage("casilla_roja.png");
-		_casiilaAzul = loadImage("casilla_azul.png");
-		_casillaVerde = loadImage("casilla_verde.png");
-		_casillaAmarilla = loadImage("casilla_amarilla.png");
 		//tablero = new JButton[20][20]; //Hardcodeado
 		
 		for (int i = 0; i < 20; i++) {
-			for (int j = 0; j < 20; j++) {
-				//this.add(new JButton());
-				
-				JButton casilla = new JButton();
-				casilla.setOpaque(true);
-				casilla.setBorderPainted(false);
-				tablero[i][j] = casilla;
-				casilla.setBackground(_COLOR_FONDO);
-				casilla.setBorderPainted(true);
-				
-				
+			for (int j = 0; j < 20; j++) {				
+				CasillaPanel casilla = new CasillaPanel(i,j);
 				this.add(casilla);
+
+				casilla.setVisible(true);
 			}
 		}
 	}
@@ -92,14 +75,4 @@ public class TableroPanel extends JPanel implements GameObserver{
 		// TODO Auto-generated method stub
 		
 	}
-	
-	private Image loadImage(String img) {
-		Image i = null;
-		try {
-			return ImageIO.read(new File("resources/" + img));
-		} catch (IOException e) {
-		}
-		return i;
-	}
-
 }
