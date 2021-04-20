154a5e35decfdd270c961424555b0aad9729e803
luisolUCM <luisolis@ucm.es>
2021-04-20 12:08:03 +0200

    .

--- Blokus/src/view/TableroPanel.java
+++ Blokus/src/view/TableroPanel.java
@@ -1,12 +1,17 @@
 package view;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
 import javax.imageio.ImageIO;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import controller.Controller;
@@ -15,14 +20,17 @@ import model.Jugador;
 
 public class TableroPanel extends JPanel implements GameObserver{
 	private static final long serialVersionUID = 1L;
+	private static final Color _COLOR_FONDO = Color.WHITE;
 	
+	private JButton tablero[][];
 	private Image _casillaVacia;
 	private Image _casillaRoja;
 	private Image _casiilaAzul;
 	private Image _casillaVerde;
 	private Image _casillaAmarilla;
 	
-	TableroPanel(Controller ctrl) {
+	
+	public TableroPanel(Controller ctrl) {
 		initGUI();
 		ctrl.addObserver(this);
 	}
@@ -30,14 +38,21 @@ public class TableroPanel extends JPanel implements GameObserver{
 
 	private void initGUI() {
 		// TODO Auto-generated method stub
-		_casillaVacia = loadImage("");
-		_casillaRoja = loadImage("");
-		_casiilaAzul = loadImage("");
-		_casillaVerde = loadImage("");
-		_casillaAmarilla = loadImage("");
+		_casillaVacia = loadImage("casilla_vacia");
+		_casillaRoja = loadImage("casilla_roja");
+		_casiilaAzul = loadImage("casilla_azul");
+		_casillaVerde = loadImage("casilla_verde");
+		_casillaAmarilla = loadImage("casilla_amarilla");
+		tablero = new JButton[20][20]; //Hardcodeado
 	}
 
-
+	//Nunca llamamos a esta funcion directamente, el sistema la llama por nosotros.
+	public void paintComponent(Graphics g) {
+		g.fillRect(100, 100, 400, 400);
+		g.clearRect(100, 100, 50, 50);
+	
+	}
+	
 	@Override
 	public void onReset(List<Jugador> jugadores, HashMap<String, String> mapaCasillas) {
 		// TODO Auto-generated method stub
